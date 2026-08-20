0c48c52b27ce0955ab010012a60dd8b3bef2ca28
BezRafael <rafaelb120934@gmail.com>
2026-08-19 21:16:27 -0300

    enviando projeto inicial, comentar códigos existentes e dar continuidade no desenvolvimento

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+# tela inicial de apresentação
+
+
+
+# abas na barra lateral esquerda
+pages = {
+    "Seções":[
+        st.Page("pages/copiloto_tmo.py", title="TMO Copilot"),
+        st.Page("pages/inserção_mobilização.py", title="Mobilização"),
+        st.Page("pages/inserção_sobrevida.py", title="Sobrevida")
+    ],
+    "Problemas?":[
+        st.Page("pages/abrir_chamado.py", title="Abrir Chamado")
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
